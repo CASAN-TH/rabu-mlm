cfcbcc6cb086579b2ff97f7b2b0f39dad6b667fc
Natsulelou <nutshapon_lelou@hotmail.com>
2019-02-25 11:06:46 +0700

    init

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
+})
+export class HomeComponent implements OnInit {
+
+  images: any = [
+    {
+      "url": "https://cdn.pixabay.com/photo/2017/01/26/23/48/love-2011994_1280.png"
+    },
+    {
+      "url": "https://image.shutterstock.com/z/stock-photo-group-portrait-of-two-white-caucasian-cute-adorable-funny-children-toddlers-sitting-together-617915693.jpg"
+    },
+    {
+      "url": "https://cdn.pixabay.com/photo/2015/09/10/16/19/alive-934671_1280.jpg"
+    }
+  ]
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  environment: "dev",
+  appName: "rabu-mlm",
+  apiUrl: "https://apis.rabu-mlm.com"
+};
