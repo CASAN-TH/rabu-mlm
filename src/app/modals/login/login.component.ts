afc375f13853f75cdd8f683cfa9a9ec9cd3b63a5
unknown <boom.jaidee@hotmail.com>
2019-02-25 12:35:26 +0700

    Puul

--- src/app/modals/login/login.component.ts
+++ src/app/modals/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider, LinkedInLoginProvider } from "angularx-social-login"
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  dataUser: any;
+  private user: SocialUser;
+  private loggedIn: boolean;
+  constructor(
+    private authService: AuthService,
+  ) { }
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
   ngOnInit() {
   }
+  openHome() {
+    this.signInWithFB();
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.user.photoUrl = "https://graph.facebook.com/" + this.user.id + "/picture?width=2000&height=2000"
+      window.localStorage.setItem('@user', JSON.stringify(this.user));
 
+      this.dataUser = JSON.parse(window.localStorage.getItem('@user'));
+
+      this.loggedIn = (user != null);
+    });
+
+  }
 }
