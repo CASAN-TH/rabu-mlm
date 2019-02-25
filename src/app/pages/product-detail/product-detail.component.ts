bcc1417158399e1a9b276f9e95378a22eb94c738
unknown <boom.jaidee@hotmail.com>
2019-02-25 16:37:07 +0700

    pusH

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { LoginModalsComponent } from './../../modals/login-modals/login-modals.component';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
@@ -9,34 +10,34 @@ import { MatDialog } from '@angular/material';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-
+  user: any;
   productDetail: any = {
-    data:{
+    data: {
       name: "ทดสอบลิปติกแดง",
       price: "120",
       shipingfee: [{
         _id: "001",
         name: "ไปรษณีไทย",
         price: "50"
-      },{
+      }, {
         _id: "002",
         name: "Kerry",
         price: "60"
       }],
-      images:[{
+      images: [{
         "url": "https://cf.shopee.co.th/file/a1089bef5b9230f470fb9493ace9dbde"
-      },{
+      }, {
         "url": "https://cf.shopee.co.th/file/5587aeb773e8633236061880e481279a"
-      },{
+      }, {
         "url": "https://cf.shopee.co.th/file/d93b05570158112438f4544fd0cea81a"
       }],
       type: [{
         name: "สีชมพู"
-      },{
+      }, {
         name: "สีน้ำเงิน"
-      },{
+      }, {
         name: "สีแดง"
-      },{
+      }, {
         name: "สีเขียว"
       }]
 
@@ -51,21 +52,31 @@ export class ProductDetailComponent implements OnInit {
     _id: ""
   };
 
-  constructor(public dialog: MatDialog) { }
+  constructor(
+    public dialog: MatDialog,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+    this.user = JSON.parse(window.localStorage.getItem('@user'));
+    console.log('loguser' + this.user);
   }
   openDialog(): void {
-    const dialogRef = this.dialog.open(LoginModalsComponent, {
-      width: '260px',
-      height: '150px',
-      data: {}
-    });
+    if (!this.user) {
+      const dialogRef = this.dialog.open(LoginModalsComponent, {
+        width: '260px',
+        height: '150px',
+        data: {}
+      });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      // this.animal = result;
-    });
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        // this.animal = result;
+      });
+    } else {
+      this.router.navigate(['/cart']);
+    }
+
   }
 
   selectType(item) {
